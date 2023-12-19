f291c48067f720fcc62d164b7b049357b8f6b58e
nwachiii <egreatness1@gmail.com>
2023-12-19 16:06:03 +0100

    fix: city list breaking

--- src/services/LocationService.js
+++ src/services/LocationService.js
@@ -6,10 +6,11 @@ const geonamesUsername = 'nwachiii';
 export async function getLargestCities() {
   try {
     const response = await axios.get(
-      `http://secure.geonames.org/searchJSON?username=${geonamesUsername}&featureClass=P&featureCode=PPLA&maxRows=15&orderby=population`
+      `https://secure.geonames.org/searchJSON?username=${geonamesUsername}&featureClass=P&featureCode=PPLA&maxRows=15&orderby=population`
     );
     return response.data.geonames;
   } catch (error) {
    <CustomToast title='An error occured' description={error.response.data.status.message || 'City not found. Please check the city name and try again.'} />;
   }
 }
+
--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -5,7 +5,7 @@ const apiKey = 'ed76e103897ad148e15e35d33729367b';
 
 export async function getWeather(cityName) {
 	try {
-		const response = await axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${cityName}`);
+		const response = await axios.get(`https://api.weatherstack.com/current?access_key=${apiKey}&query=${cityName}`);
 		return response.data;
 	} catch (error) {
 		 <CustomToast title='An error occured' description={error.response.data.status.message || 'City not found. Please check the city name and try again.'} />;
